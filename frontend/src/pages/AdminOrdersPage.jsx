a83c2392225879ca75eb5371fa00dad0edf1722a
samy246 <sdurai246@gmail.com  git config --global user.name Your>
2025-04-25 22:38:59 +0530

    cartreplaceap25

--- frontend/src/pages/AdminOrdersPage.jsx
+++ frontend/src/pages/AdminOrdersPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AdminOrders } from '../features/admin/components/AdminOrders'
 import {Navbar} from '../features/navigation/components/Navbar'
-
+import { useMeta } from '../hooks/useMeta'
 export const AdminOrdersPage = () => {
     useMeta({
               title: "Admin -Orders Page | Thekkady Spices",
