1374ac2ba739724ce5be6923f86da9f1f080b064
samy246 <sdurai246@gmail.com  git config --global user.name Your>
2025-04-25 22:35:29 +0530

    ap25admin

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { Navbar } from '../features/navigation/components/Navbar'
 import { AdminDashBoard } from '../features/admin/components/AdminDashBoard'
 import { Stack, Typography,Grid,Box, useMediaQuery, useTheme } from '@mui/material'
+import { useMeta } from '../hooks/useMeta'
 export const AdminDashboardPage = () => {
       const theme=useTheme()
       const is600=useMediaQuery(theme.breakpoints.down(600))
+        useMeta({
+            title: "Admin DashBoard | Thekkady Spices",
+            description: "Thekkady Spices"
+          });
   return (
     <>
     <Navbar isProductList={true}/>
